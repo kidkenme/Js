2ef3c237049c0d284bde44e65551447e990f4a8f
kidkenme <kidkenme@gmail.com>
2023-11-21 16:03:35 +0530

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version(all the code you write will be trated with new standards through the js engine )
+
+// alert(hi) // we are using nodejs not browser so it wont work
+
+let name = "harsh"   // string
+let age = 4          // number
+let isHappy = false  // boolean
+
+// number => 2^53
+// bigint 
+// string => "hello"
+// boolean => true/false
+// null => standalone value and its an object
+// undefined => value is not assigned yet
+// symbol => for uniqueness
+// object
+
+console.log(typeof null) // o/p: object
+console.log(typeof undefined) // o/p: undefined
+
+
+
--- basics/variables.js
+++ basics/variables.js
@@ -18,4 +18,3 @@ let newVariable;
 
 console.table([name, accountId, email, someRandomVar]);
 
-
