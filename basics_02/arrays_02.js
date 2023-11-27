88030e6380de528c48d8e7765a8718e4e196ee4a
kidkenme <kidkenme@gmail.com>
2023-11-27 15:36:09 +0530

    arrays part 2

---
+++ basics_02/arrays_02.js
@@ -0,0 +1,44 @@
+const marvel = ["hulk", "thor"]
+
+const dc = ["superman", "batman"]
+
+//marvel.push(dc)
+//console.log(marvel); // array inside an array [ 'hulk', 'thor', [ 'superman', 'batman' ] ]
+
+
+const allHeros = marvel.concat(dc)
+console.log(allHeros); // returns a single array [ 'hulk', 'thor', 'superman', 'batman' ]
+
+const allHeros2 = [...marvel, ...dc] //(spread operator) most preffered way to merge two or more arrays (... spreads the elments of the array)
+console.log(allHeros2);
+
+
+//flat
+
+const some_array = [1, 2, 3, 4, [5, 6], 7, [8, [9, 10]], 11]
+
+const flatArray = some_array.flat(Infinity) // infinity is basically the depth 
+
+console.log(flatArray); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+
+
+console.log(Array.isArray(flatArray));
+
+console.log(Array.from("hello")); // converts the string into an array [ 'h', 'e', 'l', 'l', 'o' ]
+
+console.log(Array.from({name : "harsh" })); // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3)); // [ 100, 200, 300 ]
+
+
+
+
+
+
+
+
